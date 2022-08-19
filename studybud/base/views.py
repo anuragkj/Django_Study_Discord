134721baf94be75e7c99ce2449c7910ea2b1a352
anuragkj <f20200128@dubai.bits-pilani.ac.in>
2022-08-19 20:19:04 +0530

    dynamic value system added to rooms

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+
+rooms = [
+    {'id':1, 'name':'Lets learn python!'},
+    {'id':2, 'name':'Design with me!'},
+    {'id':3, 'name':'Front end development'},
+]
 
 def home(request):
-    return HttpResponse('Home page')
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return HttpResponse('ROOM')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i["id"] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
