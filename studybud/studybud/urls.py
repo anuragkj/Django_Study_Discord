1871d62428cf1ce055e84bb47af4dcde5c2892eb
anuragkj <f20200128@dubai.bits-pilani.ac.in>
2022-08-19 19:36:08 +0530

    r

--- studybud/studybud/urls.py
+++ studybud/studybud/urls.py
@@ -5,7 +5,12 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse('Home page')
+
+def room(request):
+    return HttpResponse('ROOM')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
+    path('room/', room),
 ]
